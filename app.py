ad2e12a653a6ca276b94ece16ca737fd901493c0
Mark Musil <musilmark25@gmail.com>
2024-01-21 08:40:59 -0500

    add simple matplotlib

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1,2])
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    
+    return f"<img src='data:image/png;base64,{data}'/>"
